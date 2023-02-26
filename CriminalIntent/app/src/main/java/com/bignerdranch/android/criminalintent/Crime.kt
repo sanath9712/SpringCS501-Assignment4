1884734419395670565b8245e630955bf742da3b
sanathbhimsen <sanathbhimsen26@gmail.com>
2023-02-26 12:29:58 -0500

    Added the base code

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean,
+    var requiresPolice: Boolean
+)
+
